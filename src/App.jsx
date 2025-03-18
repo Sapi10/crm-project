273d2fa06d9eb8c571d275d976ca8b05198c59d7
Sapi10 <134209657+Sapi10@users.noreply.github.com>
2025-03-18 15:25:23 +0530

    Initial commit: Setup Vite + React project with Tailwind CSS v3

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='container mx-auto text-center shadow-lg rounded bg-gray-200 p-4'>
+      <h1 className=' text-blue-500 text-xl '>Hello React.</h1>
+      <p className='text-red-400 italic '> I'm using Vite + Taiwind CSS verion 3 for this project.</p>
+    </div>
+  )
+}
+
+export default App
